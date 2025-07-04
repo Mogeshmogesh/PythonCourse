abb161db7d1bfdebd6ec41a4f10f5c19096b1ad0
Mogeshmogesh <144776908+Mogeshmogesh@users.noreply.github.com>
2025-07-04 06:58:41 -0700

    Add files via upload

---
+++ atmcashdeposit.py
@@ -0,0 +1,13 @@
+# python program
+# atm demo ncode
+def cashdeposit(car,bus):
+    added=car+bus
+    return added
+if __name__ == "__main__":
+    print("welcome to cashdeposit system")
+    balance=5000
+    cash_deposit=5000
+    newbalance=cashdeposit(balance,cash_deposit)
+    print(newbalance)
+
+
