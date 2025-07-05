a54df7f51948eab1642f82912e5e341eec00cc97
Mogeshmogesh <144776908+Mogeshmogesh@users.noreply.github.com>
2025-07-05 18:32:09 +0530

    Add files via upload

---
+++ atmbalancecheck.py
@@ -0,0 +1,10 @@
+# python program
+# atm demo code
+def check_balance(balance):
+    print("your current balance is:Rs.",balance)
+def main():
+    balance=250000
+    check_balance(balance)
+
+if __name__ == "__main__":
+    main()
